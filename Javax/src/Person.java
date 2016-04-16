e0fba66946bcce19c3262e74276fa1294e891971
Rares Bucur <ionutraresbucur2@gmail.com>
2016-04-16 12:29:10 +0300

    16.04.2016 work work

---
+++ Javax/src/Person.java
@@ -0,0 +1,31 @@
+/**
+ * Created by icondor on 16/04/16.
+ */
+public class Person {
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    private String name;
+    private String phoneNumber;
+
+
+    public Person(String n, String p) {
+        name=n;
+        phoneNumber=p;
+    }
+
+
+}
